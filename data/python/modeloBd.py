fdad0553f991e241a94a77dd71ec53d5796e6908
kralsito <franco_kral@hotmail.com>
2024-05-25 18:05:27 -0300

    modificacion patrones

--- data/python/modeloBd.py
+++ data/python/modeloBd.py
@@ -38,3 +38,122 @@ class Comentario(models.Model):
     descripcion = models.TextField(max_length=100)
     creado_el = models.DateTimeField(auto_now_add=True) 
     posteo= models.ForeignKey(Post,on_delete=models.CASCADE, null=False, blank=False, related_name="comentarios")
+
+    from sqlite3 import IntegrityError
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.contrib.auth.models import Group, Permission
+from django.http import JsonResponse
+from django.utils import timezone
+from django.contrib.auth import get_user_model
+# Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('El Email es obligatorio')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, password, **extra_fields)
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    perfil = models.OneToOneField('Perfil', on_delete=models.CASCADE, null=True, blank=True)
+
+    # Agregar related_name personalizado para evitar conflictos
+    groups = models.ManyToManyField(
+        Group,
+        verbose_name='groups',
+        blank=True,
+        related_name='custom_user_set',
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        verbose_name='user permissions',
+        blank=True,
+        related_name='custom_user_set',
+    )
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+def registrar_usuario(request):
+    if request.method == 'POST':
+        email = request.data.get('email')
+        name = request.data.get('name')
+        apellido = request.data.get('apellido')
+        telefono = request.data.get('telefono')
+        provincia = request.data.get('provincia')
+        localidad = request.data.get('localidad')
+
+        try:
+            user = User.objects.create_user(email=email, password='password')
+            perfil = Perfil.objects.create(
+                usuario=user,
+                nombrePerfil=name,
+                apellidoPerfil=apellido,
+                telefono=telefono,
+                localidad=localidad
+            )
+            user.perfil = perfil  # Asignar el perfil al usuario
+            user.save()  # Guardar el usuario con el perfil asignado
+            return JsonResponse({'mensaje': 'Usuario registrado exitosamente'})
+        except IntegrityError:
+            return JsonResponse({'mensaje': 'Correo duplicado'}, status=400)
+   
+class Publicacion(models.Model):
+    usuario = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True) #Esto es lo que modifique para poder eliminar
+    nombrePerro = models.CharField(max_length=30)
+    fotoPerro = models.ImageField(upload_to='publicaciones',null=True, blank=True)
+    edadPerro = models.CharField(max_length=20)
+    SEXO_CHOICES = [
+        ('Macho', 'Macho'),
+        ('Hembra', 'Hembra'),
+    ]
+    sexoPerro = models.CharField(max_length=15, choices=SEXO_CHOICES)
+
+    TAMANO_CHOICES = [
+        ('Pequeño', 'Pequeño'),
+        ('Mediano', 'Mediano'),
+        ('Grande', 'Grande'),
+    ]
+    tamanioPerro = models.CharField(max_length=10, choices=TAMANO_CHOICES)
+
+    BOOLEAN_CHOICES = [
+        ('Si', 'Si'),
+        ('No', 'No'),
+    ]
+    desparasitadoPerro = models.CharField(max_length=5, choices=BOOLEAN_CHOICES, default='No')
+    castradoPerro = models.CharField(max_length=5, choices=BOOLEAN_CHOICES, default='No')
+    libretaPerro = models.CharField(max_length=5, choices=BOOLEAN_CHOICES, default='No')
+    vacunadoPerro = models.CharField(max_length=5, choices=BOOLEAN_CHOICES, default='No')
+
+    #Funcion para retornar algo cuando llamo al objeto
+    def __str__(self):
+        return self.nombrePerro
+    
+class Perfil(models.Model):
+    nombrePerfil = models.CharField(max_length=30)
+    apellidoPerfil = models.CharField(max_length=30)
+    localidad = models.CharField(max_length=30)
+    telefono = models.IntegerField()
+    biografia = models.CharField(max_length=200, null=True)
+    fotoPerfil = models.ImageField(upload_to='perfiles',null=True, blank=True)
+    def __str__(self):
+        return f"Perfil de: {self.nombrePerfil}"
+
+class Favorito(models.Model):
+    usuario = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    publicacion = models.ForeignKey(Publicacion, on_delete=models.CASCADE)
--- data/python/predecirtemporales.py
+++ data/python/predecirtemporales.py
@@ -42,3 +42,59 @@ plt.plot(y, label='True')
 plt.plot(y_pred, label='Predicted')
 plt.legend()
 plt.show()
+
+from keras._tf_keras.keras.models import load_model
+from keras.src.legacy.preprocessing.text import Tokenizer
+from keras.src.utils.sequence_utils import pad_sequences
+import numpy as np
+import pickle
+
+# Cargar el modelo y el tokenizer
+model = load_model("model/language_model.h5")
+
+# Cargar los diccionarios y la longitud máxima de secuencia
+with open("model/tokenizer.pickle", "rb") as handle:
+    tokenizer = pickle.load(handle)
+with open("model/label_to_index.pickle", "rb") as handle:
+    label_to_index = pickle.load(handle)
+with open("model/index_to_label.pickle", "rb") as handle:
+    index_to_label = pickle.load(handle)
+with open("model/max_length.pickle", "rb") as handle:
+    max_length = pickle.load(handle)
+
+# Función para predecir el lenguaje de programación
+def predict_language(code):
+    sequence = tokenizer.texts_to_sequences([code])
+    padded_sequence = pad_sequences(sequence, maxlen=max_length)
+    prediction = model.predict(padded_sequence)
+    language_index = np.argmax(prediction)
+    return index_to_label[language_index]
+
+# Predicción del lenguaje de programación de un archivo de texto
+# Ruta al archivo de texto
+file_path = "input.txt"
+
+# Leer el contenido del archivo
+with open(file_path, "r") as file:
+    code_example = file.read()
+
+predicted_language = predict_language(code_example)
+print(f"El lenguaje de programacion es: {predicted_language}")
+
+
+# Función para predecir las probabilidades de cada lenguaje de programación para un fragmento de código dado
+def predict_language_probabilities(code):
+    # Tokenizar el código
+    sequence = tokenizer.texts_to_sequences([code])
+    padded_sequence = pad_sequences(sequence, maxlen=max_length)
+    # Realizar la predicción
+    prediction = model.predict(padded_sequence)[0]
+    # Obtener las probabilidades de cada lenguaje
+    probabilities = {index_to_label[i]: prob for i, prob in enumerate(prediction)}
+    return probabilities    
+
+probabilities = predict_language_probabilities(code_example)
+for language, probability in probabilities.items():
+    print(f"{language}: {probability:.2f}%")
+
+    
